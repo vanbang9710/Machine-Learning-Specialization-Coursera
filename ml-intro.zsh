6430b98068fcda6078699db628e5a3c2aed02970
BeztDonkey <vanbang9710@gmail.com>
2023-09-27 22:38:43 +0700

    minor

--- ml-intro.zsh
+++ ml-intro.zsh
@@ -1,7 +1,7 @@
 conda create -n ml -c conda-forge ipykernel scikit-learn numpy matplotlib
-# ipywidgets ipympl sympy pandas networkx pydot xgboost tabulate pickle5 basemap imageio statsmodels pyvirtualdisplay box2d-py pygame
+# ipywidgets ipympl sympy pandas networkx pydot xgboost tabulate pickle5 basemap imageio imageio-ffmpeg statsmodels pyvirtualdisplay box2d-py pygame
 conda activate ml
-conda install -n ml -c conda-forge imageio-ffmpeg
+conda install -n ml -c conda-forge tqdm
 # conda install -n ml -c conda-forge gym=0.24.0
 # conda install -n ml -c "conda-forge/label/broken" gym=0.24.0
 pip install tensorflow
